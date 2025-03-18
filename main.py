edb994716f419d8f79eef86b29decec496a242f0
student05 <student@student.com>
2025-03-18 08:31:27 +0100

    cos

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import sys
+import random
+import string
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt6.QtWidgets import QDialog, QApplication, QMessageBox
+import layout
+
+class MyWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = layout.Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.buttonGenerate.clicked.connect(self.generate_password)
+        self.ui.zatwierdz.clicked.connect(self.zatwierdz)
+        self.show()
+
+    def generate_password(self):
+        num_chars = self.ui.spinBox.value()
+
+        lowercase_letters = string.ascii_lowercase
+        uppercase_letters = string.ascii_uppercase
+        digits = string.digits
+        special_chars = "!@#$%^&*()_+-="
+
+        char_set = lowercase_letters
+
+        if self.ui.checkUperLower.isChecked():
+            char_set += uppercase_letters
+        if self.ui.checkCyfry.isChecked():
+            char_set += digits
+        if self.ui.checkSpecialCharacters.isChecked():
+            char_set += special_chars
+
+        password = [random.choice(digits)] if self.ui.checkCyfry.isChecked() else []
+
+        if self.ui.checkSpecialCharacters.isChecked():
+            password.append(random.choice(special_chars))
+
+        password += random.choices(char_set, k=num_chars - len(password))
+
+        random.shuffle(password)
+
+        password = ''.join(password)
+
+        QMessageBox.information(self, "Wygenerowane hasło", f"Twoje hasło: {password}")
+
+    def zatwierdz(self):
+        imie = self.ui.EditImie.text()
+        nazwisko = self.ui.EditNazwisko.text()
+
+        stanowisko = self.ui.comboBox.currentText()
+
+        password = self.generate_password()
+
+        message = f"Dane pracownika:\nImię: {imie}\nNazwisko: {nazwisko}\nStanowisko: {stanowisko}\nHasło: {password}"
+        QMessageBox.information(self, "Dane pracownika", message)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec())
